3167fc699ea5b2a3c16815b6c5a78ecc76a84dd0
alexzlov <zlov.ehl@gmail.com>
2014-07-21 22:56:14 +0700

    editline

--- repl.c
+++ repl.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
 
 /* Static buffer for user input */
 static char input[2048];
@@ -9,9 +12,10 @@ int main(int argc, char** argv) {
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
-        fputs("repl> ", stdout);
-        fgets(input, 2048, stdin);
-        printf("Got it: %s", input);
+        char* input = readline("repl> ");
+        add_history(input);
+        printf("Got it: %s\n", input);
+        free(input);
     }
 
     return 0;
