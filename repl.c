3957f3caa688c59055477e6f5cd31c460d576703
man213 <zlov.ehl@gmail.com>
2014-07-21 10:57:56 +0700

    ansi colors

--- repl.c
+++ repl.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <editline/readline.h>
+
+#include "term_colors.h"
 
 /* Static buffer for user input */
 static char input[2048];
 
 int main(int argc, char** argv) {
-    
+    printf(FG_COLOR_WHITE);
     puts("Lispy Version 0.0.1");
     puts("Press Ctrl+c to Exit\n");
+    printf(COLOR_RESET);
 
     while (1) {
-        char* input = readline("repl> ");
+        printf(FG_COLOR_YELLOW);
+        printf("repl> ");
+        char* input = readline(COLOR_RESET);
+        printf(COLOR_RESET);
         add_history(input);
         printf("Got it: %s\n", input);
         free(input);
     }
+    printf(COLOR_RESET);
 
     return 0;
 }
---
+++ term_colors.h
@@ -0,0 +1,23 @@
+/* ANSI colors */
+
+#define COLOR_RESET           "\x1b[0m"
+
+// Foreground
+#define FG_COLOR_BLACK        "\x1b[30m"
+#define FG_COLOR_RED          "\x1b[31m"
+#define FG_COLOR_GREEN        "\x1b[32m"
+#define FG_COLOR_YELLOW       "\x1b[33m"
+#define FG_COLOR_BLUE         "\x1b[34m"
+#define FG_COLOR_MAGENTA      "\x1b[35m"
+#define FG_COLOR_CYAN         "\x1b[36m"
+#define FG_COLOR_WHITE        "\x1b[37m"
+
+// Background
+#define BG_COLOR_BLACK        "\x1b[40m"
+#define BG_COLOR_RED          "\x1b[41m"
+#define BG_COLOR_GREEN        "\x1b[42m"
+#define BG_COLOR_YELLOW       "\x1b[43m"
+#define BG_COLOR_BLUE         "\x1b[44m"
+#define BG_COLOR_MAGENTA      "\x1b[35m"
+#define BG_COLOR_CYAN         "\x1b[36m"
+#define BG_COLOR_WHITE        "\x1b[37m"
